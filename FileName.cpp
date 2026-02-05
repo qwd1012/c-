5f8e9d96e6181da87f95d8c6915df6b9294bf8ad
qwd1012 <jiwbyun1012@gmail.com>
2026-02-05 13:44:23 +0900

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,170 +1,53 @@
 //#include <stdio.h>
 //
-//int main()
+//void main()
 //{
-//	int first = 1 % 3;
-//	printf("%d = 1%%3\n", first);
-//	int second = 2 % 3;
-//	printf("%d = 2%%3\n", second);
-//	int third = 3 % 3;
-//	printf("%d = 3%%3\n", third);
-//	int fourth = 4 %3;
-//	printf("%d = 4%%3\n", fourth);
-//	int fifth = 5 % 3;
-//	printf("%d = 5%%3\n", fifth);
-//	int sixth = 6 % 3;
-//	printf("%d = 6%%3\n", sixth);
-//	return 0;
-//}
-
-//#include <stdio.h>
+//	int a = 100;
 //
-//int main()
-//{
-//	int a;
-//	printf("숫자 입력: ");
-//	scanf("%d" , &a);
-//	int b = a % 50;
-//	printf("%d", b);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int data1 = 6 / 2;
-//	printf("%d = 6/2\n", data1);
-//	int data2 = 6 / 3;
-//	printf("%d = 6/3\n", data2);
-//	int data3 = 6 / 4;
-//	printf("%d = 6/4\n", data3);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int number1, number2;
-//	scanf("%d %d", &number1, &number2);
-//	printf("%d=", first);
-//	printf("%d/%d\n", number1, number2);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int coin;
-//	printf("돈을 입력해주세요.(원)");
-//	scanf("%d", &coin);
-//
-//	int coin10000 = coin / 10000;
-//	coin = coin % 10000;
-//	int coin5000 = coin / 5000;
-//	coin = coin % 5000;
-//	int coin1000 = coin / 1000;
-//	printf("만원권 %d개, ", coin10000);
-//	printf("오천원권 %d개, ", coin5000);
-//	printf("천원권 %d개, ", coin1000);
-//	printf("나머지는 동전입니다.");
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int money=1000;
-//	int jelly;
-//	printf("젤리 가격: ");
-//	scanf("%d", &jelly);
-//
-//	money -= jelly;
-//
-//	int coin500 = money / 500;
-//	money %= 500;
-//	int coin100 = money / 100;
-//	money = money % 100;
-//	int coin50 = money / 50;
-//	money = money % 50;
-//	int coin10 = money / 10;
-//	printf("500원 %d개, ", coin500);
-//	printf("100원 %d개, ", coin100);
-//	printf("50원 %d개, ", coin50);
-//	printf("10원 %d개", coin10);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main()
-//{
-//	int randInt = rand() % 10;
-//	int nextInt = rand() % 10;
-//	printf("첫숫자 : %d\n", randInt);
-//	printf("다음숫자 : %d", nextInt);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-//
-//int main()
-//{
-//	srand((unsigned int)time(NULL));
-//	int randInt = rand() % 10;
-//	int nextInt = rand() % 10+50;
-//	printf("첫숫자 : %d\n", randInt);//0~9
-//	printf("다음숫자 : %d", nextInt);//50~59
-//
-//	return 0;
-//}
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
-//int main()
-//{
-//	srand((unsigned int)time(NULL));
-//	printf("내 주머니에 있는 돈은");
-//	int mymoney = (rand() % 10 + 1) * 1000;
-//	printf("%d원이다.\n", mymoney);//1000~10000
-//	int coin500 = rand() % 4 * 500;//0~1500
-//	int coin100 = rand() % 5000;
-//	coin100 = coin100 / 100 * 100;//0~4900
-//	printf("그리고 과자의 가격은");
-//	printf("%d원이다.", coin500 + coin100);
-//	printf("\n나는 과자를 살 수 있을까?");
-//	return 0;
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int apple = 15;
-//	if (apple == 15)
+//	while (a == 200)
 //	{
-//		printf("apple은 15개 있습니다.\n");
+//		printf("while문 내부에 들어왔습니다.\n");
 //	}
-//	return 0;
-//}
-
-//#include <stdio.h>
 //
-//int main()
-//{
-//	int apple = 15;
-//	if (apple != 17)
-//	{
-//		printf("apple은 17개가 아닙니다.\n");
-//	}
-//	return 0;
+//	do {
+//		printf("do~while문 내부에 들어왔습니다.\n");
+//	} while (a == 200);
 //}
 
-#include 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//void main()
+//{
+//	int a, b;
+//
+//	while (1)
+//	{
+//		printf("더할 두 수 입력 (멈추려면 0을 입력) : ");
+//		scanf("%d %d", &a, &b);
+//
+//		if (a == 0)
+//			break;
+//
+//		printf("%d + %d = %d \n", a, b, a + b);
+//	}
+//
+//	printf("0을 입력해서 for문을 탈출했습니다.\n");
+//}
+
+#include <stdio.h>
+
+void main()
+{
+	int hap = 0;
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			continue;
+
+		hap += i;
+	}
+
+	printf("1~100까지의 합(3의 배수 제외): %d\n", hap);
+}
