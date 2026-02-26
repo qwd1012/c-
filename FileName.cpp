f7312df53c570f0e99c07b174e6c8a399352466c
qwd1012 <jiwbyun1012@gmail.com>
2026-02-26 13:01:00 +0900

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -3,40 +3,35 @@
 //{
 //	int i, k;
 //
-//	for (i = 0;i < 3;i++)
-//	{
-//		for (k = 0;k < 2;k++)
-//		{
-//			printf("중첩 for문입니다. (i값: %d, k값: %d)\n", i, k);
-//		}
-//	}
+//	for (i = 1, k = 1;i <= 9;i++, k++)
+//		printf("%d * %d = %d\n", i, k, i * k);
 //}
 
 //#include <stdio.h>
 //void main()
 //{
-//	int i, k;
-//
-//	for (i = 2;i <= 9;i++)
+//	int i;
+//	i = 0;
+//	for (;;)
 //	{
-//		for (k = 1;k <= 9;k++)
-//		{
-//			printf("%d * %d = %d \n", i, k, i * k);
-//		}
+//		printf("%d\n", i);
+//		i++;
 //	}
 //}
 
 #include <stdio.h>
 void main()
 {
-	int i, k;
+	int i;
 
-	for (i = 1;i <= 9;i++)
+	for (i = 0;i < 128;i++)
 	{
-		for (k = 2;k <= 9;k++)
+		if (1 % 16 == 0)
 		{
-			printf("%2dX%2d=%2d", k, i, i * k);
+			printf("-------------------\n");
+			printf("10진수 16진수 문자\n");
+			printf("-------------------\n");
 		}
-		printf("\n");
+		printf("%5d %5x %5c\n", i, i, i);
 	}
 }
