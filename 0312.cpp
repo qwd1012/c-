d3dfbcde428965b0cca3cfd233478b46c3270427
qwd1012 <jiwbyun1012@gmail.com>
2026-03-12 21:52:03 +0900

    Add files via upload

---
+++ 0312.cpp
@@ -0,0 +1,134 @@
+//#include <stdio.h>
+//void main()
+//{
+//	short pt1;
+//	scanf("%hd", &pt1);
+//	printf("값 : %hd\n", pt1);
+//}
+
+//#include <stdio.h>
+//
+//short getdefaultdata()
+//{
+//	short pt1;
+//	scanf("%hd", &pt1);
+//	return pt1;
+//}
+//void main()
+//{
+//	short pt;
+//	pt = getdefaultdata();
+//	printf("값 : %hd\n", pt);
+//}
+//
+//#include <stdio.h>
+//
+//struct point { short x, y; };
+//
+//short getdefaultdata();
+//void printdata(short pt2);
+//short add(short pt2);
+//
+//void main()
+//{
+//	short pt;
+//	pt = getdefaultdata();
+//	printdata(pt);
+//	pt = add(pt);
+//	printdata(pt);
+//}
+//
+//short getdefaultdata()
+//{
+//	short pt1;
+//	scanf("%hd", &pt1);
+//	return pt1;
+//}
+//
+//void printdata(short pt2)
+//{
+//	printf("값 : %hd\n", pt2);
+//}
+//
+//short add(short pt2)
+//{
+//	pt2 += 5;
+//	return pt2;
+//}
+
+
+//#include <stdio.h>
+//
+//struct POINT { short x, y; };
+//
+//POINT getdefaultdata();
+//void printdata(POINT pt2);
+//POINT add(POINT pt2);
+//
+//void main()
+//{
+//	struct POINT pt;
+//	pt = getdefaultdata();
+//	printdata(pt);
+//	pt = add(pt);
+//	printdata(pt);
+//}
+//
+//POINT getdefaultdata()
+//{
+//	POINT pt1;
+//	scanf("%hd", &pt1.x);
+//	scanf("%hd", &pt1.y);
+//	return pt1;
+//}
+//
+//void printdata(POINT pt2)
+//{
+//	printf("값 : %hd\n", pt2.x);
+//	printf("값 : %hd\n", pt2.y);
+//	
+//}
+//
+//POINT add(POINT pt2)
+//{
+//	pt2.x += 5;
+//	pt2.y += 5;
+//	return pt2;
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//	char arr[4];
+//	printf("%d\n", sizeof(arr));
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//	int arr[4];
+//	printf("%d\n", sizeof(arr));
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//	char alpah[4] = { 'a','b','c','d' };
+//	printf("%c%c%c%c%c\n", alpah[0], alpah[1], alpah[2], alpah[3]);
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//	int num[3] = { 3,6,9 };
+//	printf("num의 안에 들어있는");
+//	printf("1번째 숫자는? %d\n", num[1]);
+//}
+
+#include <stdio.h>
+int main()
+{
+	int num[3] = { 3.6.9 };
+	printf("%d번째에 %d 저장\n", 0, num[0]);
+	printf("")
+}
