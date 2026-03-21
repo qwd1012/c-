3427c18f586642dfc205bfa6cd6fae27d0517b14
qwd1012 <jiwbyun1012@gmail.com>
2026-03-21 18:11:09 +0900

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,96 +1,39 @@
-//#define _CRT_SECURE_NO_WARNINGS
 //#include <stdio.h>
-//
 //void main()
 //{
-//    int a, b, c, d;
-//    int hap;
+//	int aa[100], bb[100];
+//	int i;
 //
-//    printf("1번째 숫자를 입력하세요 : ");
-//    scanf("%d", &a);
-//
-//    printf("2번째 숫자를 입력하세요 : ");
-//    scanf("%d", &b);
-//
-//    printf("3번째 숫자를 입력하세요 : ");
-//    scanf("%d", &c);
-//
-//    printf("4번째 숫자를 입력하세요 : ");
-//    scanf("%d", &d);
-//
-//    hap = a + b + c + d;
-//
-//    printf("합계 ==> %d \n", hap);
+//	for (i = 0;i < 100;i++)
+//	{
+//		aa[i] = i * 2;
+//	}
+//	for (i = 0;i < 100;i++)
+//	{
+//		bb[i] = aa[99 - i];
+//	}
+//	printf("bb[0]는 %d, bb[99]는 %d 입력됨\n", bb[0], bb[99]);
 //}
 
-//#define _CRT_SECURE_NO_WARNINGS
 //#include <stdio.h>
-//
 //void main()
 //{
-//    int aa[4];
-//    int hap;
-//
-//    printf("1번째 숫자를 입력하세요 : ");
-//    scanf("%d", &aa[0]);
-//
-//    printf("2번째 숫자를 입력하세요 : ");
-//    scanf("%d", &aa[1]);
-//
-//    printf("3번째 숫자를 입력하세요 : ");
-//    scanf("%d", &aa[2]);
-//
-//    printf("4번째 숫자를 입력하세요 : ");
-//    scanf("%d", &aa[3]);
-//
-//    hap = aa[0] + aa[1] + aa[2] + aa[3];
-//
-//    printf("합계 ==> %d \n", hap);
+//	int aa[] = { 10,20,30,40,50 };
+//	int count;
+//	count = sizeof(aa) / sizeof(int);
+//	printf("배열 aa[]의 요소의 개수는 %d개 입니다\n",count);
 //}
 
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//void main()
-//{
-//    int aa[4];
-//    int hap = 0;
-//    int i;
-//
-//    for (i = 0; i <= 3; i++)
-//    {
-//        printf("%d번째 숫자를 입력하세요 : ", i + 1);
-//        scanf("%d", &aa[i]);
-//    }
-//
-//    hap = aa[0] + aa[1] + aa[2] + aa[3];
-//
-//    printf("합계 ==> %d \n", hap);
-//}
 
 #include <stdio.h>
-
 void main()
 {
-    int aa[4] = { 100, 200, 300, 400 };
-    int bb[] = { 100, 200, 300, 400 };
-    int cc[4] = { 100, 200 };
-    int dd[4] = { 0 };
-    int i;
-
-    for (i = 0; i <= 3; i++)
-        printf("aa[%d]==>%d\t", i, aa[i]);
-    printf("\n");
-
-    for (i = 0; i <= 3; i++)
-        printf("bb[%d]==>%d\t", i, bb[i]);
-    printf("\n");
-
-    for (i = 0; i <= 3; i++)
-        printf("cc[%d]==>%d\t", i, cc[i]);
-    printf("\n");
-
-    for (i = 0; i <= 3; i++)
-        printf("dd[%d]==>%d\t", i, dd[i]);
-    printf("\n");
+	char ss[8] = "Basic-C";
+	int i;
+	ss[5] = '#';
+	for (i = 0;i < 8;i++)
+	{
+		printf("ss[%d] ==> %c\n", i, ss[i]);
+	}
+	printf("문자열 배열 ss ==> %s\n", ss);
 }
