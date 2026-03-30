26365c584f91421bff3def9cfd25996c7ec469d1
qwd1012 <jiwbyun1012@gmail.com>
2026-03-30 21:53:39 +0900

    Add files via upload

---
+++ 0330.cpp
@@ -0,0 +1,52 @@
+//#include <stdio.h>
+//int main()
+//{
+//	const int n = 10;
+//	int arr[n][n] = {};
+//	int num = 1;
+//	int x =-1;
+//	int y = 0;
+//	int k = n;
+//	int num1 = 1;
+//	
+//	while (1) {
+//		for (int i = 0; i < k; i++)
+//		{
+//			x=x+num1;
+//			arr[y][x] = num;
+//			num++;
+//		}
+//		k--;
+//		if (k == 0)
+//			break;
+//		for (int i = 0; i < k; i++)
+//		{
+//			y = y + num1;
+//			arr[y][x] = num;
+//			num++;
+//		}num1 = -num1;
+//		
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			printf("%3d", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//		
+//}
+
+#include <stdio.h>
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	int arr[]
+	for (int i = 0; i < num; i++)
+	{
+		scanf("%d")
+	}
+}
